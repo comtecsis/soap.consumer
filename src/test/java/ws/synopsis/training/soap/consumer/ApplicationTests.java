604485829ca5b298631b6451de583bffdfb04f92
Elvis Perez <ebardalesdj@mpfn.gob.pe>
2021-09-20 16:19:46 -0500

    Commit inicial

---
+++ src/test/java/ws/synopsis/training/soap/consumer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.synopsis.training.soap.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
